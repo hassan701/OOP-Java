fe33a275c3021ae6ac3dedd9083cb17684b6f76a
hassan701 <71362199+hassan701@users.noreply.github.com>
2021-05-18 17:25:00 +0300

    ex

---
+++ May 4+5/Launch.java
@@ -0,0 +1,38 @@
+class Knightmare {
+	public void name() {
+		System.out.print("This Knightmare Frame is named:...\n");
+	}
+	public void name(String name) {
+		System.out.print("This Knightmare Frame is named:"+name+"\n");
+	}
+	public void Genration() {
+		System.out.print("This is a ... Genration Knightmare Frame \n");
+	}
+	public void Pilot() {
+		System.out.print("This Knightmare Frame is currently piloted by:...\n");
+	}
+	
+
+}
+
+class Lancelot extends Knightmare {
+	
+	public void Genration() {
+		System.out.print("This is a 7th Genration Knightmare Frame\n");
+	}
+	@Override 
+	public void Pilot() {
+		System.out.print("This Knightmare Frame is currently piloted by: Suzaku Kururugi\n");
+	}
+}
+
+
+public class Launch {
+ public static void main(String[] args)
+ {
+	 Knightmare test = new Knightmare();
+     test.name("Lancelot");
+     Lancelot lancelot = new Lancelot();
+     lancelot.Pilot();
+ }
+}
